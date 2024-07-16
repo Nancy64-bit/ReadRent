488fe24167af2c00725c20ee50be661be1a6f671
Nandini Chauhan <hackergwen@proton.me>
2024-07-16 23:56:06 +0530

    init commit

---
+++ src/main/java/com/crio/read_rent/ReadRentApplication.java
@@ -0,0 +1,13 @@
+package com.crio.read_rent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crio/read_rent/ReadRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crio.read_rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
